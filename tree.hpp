dd91030862add200b1e9a9fbb598343fa6b087be
Sori Kim <sorikim98@naver.com>
2022-07-24 21:35:14 +0900

    restart tree

--- tree.hpp
+++ tree.hpp
@@ -4,46 +4,13 @@
 namespace ft 
 {
 	template <class T>
-	struct Node {
-		T data;
-		node *left;
-		node *right;
-		int balance_factor;
-	};
-
-	class AVL {
-		private: 
-			Node* root;
-
+	class Node
+	{
 		public:
-
-			void insert(const T value)
-			{
-				if (root == NULL)
-				{
-					root = new Node;
-					root->data = value;
-					root->left = NULL;
-					root->right = NULL;
-				} 
-				else if (value < root->data)
-				{
-					root->left = insert(root->left, value);
-					root = balance(root);
-				}
-				else 
-				{
-					root->right = insert(root->right, value);
-					root = balance(root);
-				}
-			}
-
-			Node* balance(Node *root)
-			{
-				this->balce_factor = diff(root);
-				if (factor > 1)
-			}
-
+			typedef T value_type;
+			
+		
+		
 
 	}
 }
